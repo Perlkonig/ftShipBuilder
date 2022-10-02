69eea442885f66ea5d7ae0ef3fea4b33008e1464
Elliot Francis <ef@windowlatch.org>
2022-10-02 12:09:03 -0600

    Added Screen Level Control Button

--- src/components/SysDisplay/AA.svelte
+++ src/components/SysDisplay/AA.svelte
@@ -26,6 +26,15 @@
         sys.area = ! sys.area;
         $ship = $ship;
     };
+
+    const toggleLevel = () => {
+        if (! sys.hasOwnProperty("level")) {
+            sys.level = 1;
+        }
+        console.log(sys)
+        sys.level = (sys.level == 1 ? 2 : 1);
+        $ship = $ship;
+    };
 </script>
 
 <nav class="level is-mobile">
@@ -42,6 +51,13 @@
             <span class="icon is-small" title="Toggle Area Mode"><i class="fa-solid fa-minimize"></i></span>
         {:else}
             <span class="icon is-small" title="Toggle Area Mode"><i class="fa-solid fa-maximize"></i></span>
+        {/if}
+      </button>
+      <button class="button level-item is-small" on:click="{toggleLevel}">
+        {#if ( (sys.hasOwnProperty("level")) && (sys.level) ) }
+            <span class="icon is-small" title="Toggle Screen Level">L{sys.level}</span>
+        {:else}
+            <span class="icon is-small" title="Toggle Screen Level">(LM)</span>
         {/if}
       </button>
     </div>
