20b9c0eae3da22e6b603560d6ed90121994b0c20
Aaron Dalton <aaron@daltons.ca>
2023-04-07 18:21:44 -0600

    Added missing error messages, even though they should never appear in normal usage.

--- src/components/Status.svelte
+++ src/components/Status.svelte
@@ -19,16 +19,21 @@
     });
 
     const errorMsgs: Map<EvalErrorCode, string> = new Map([
-        // [EvalErrorCode.NoMass, "A ship needs mass."],
+        [EvalErrorCode.NoMass, "You haven't specified a mass for your ship."],
         [EvalErrorCode.BadMass, "The ship's mass must be between 5 and 300."],
+        [EvalErrorCode.OverMass, "You have equipped more systems than your ship's mass can accommodate."],
         [EvalErrorCode.LowHull, "The ship's hull must be at least 10% of total mass."],
-        // [EvalErrorCode.OverShell, "You have too many rows of shell armour. You cannot have more than 5."],
+        [EvalErrorCode.OverShell, "You have too many rows of shell armour. You cannot have more than 5."],
         [EvalErrorCode.OverArmour, "One of your rows of armour is too long. You can't have more armour than you have hull in your top row."],
         [EvalErrorCode.OverDCP, "You have allocated too many damage control parties."],
         [EvalErrorCode.OverMarine, "You have allocated too many onboard marines."],
         [EvalErrorCode.OverCrew, "You have overallocated your crew. You may need to add some berths."],
         [EvalErrorCode.OverSpinal, "Your spinal-mounted weapons are too heavy. You can only equip 16 mass of spinal weapons for every 50 total ship mass (rounded up)."],
         [EvalErrorCode.OverTurret, "You have too many turrets. You can only have one turret for every 50 total ship mass (rounded up)."],
+        [EvalErrorCode.DblUID, "At least one of your systems shares a UUID with another. This should not happen in the normal course of using the builder. Did you modify a JSON file?"],
+        [EvalErrorCode.OverPBL, "You have equipped more plasma bolt launchers than your ship's mass can accommodate."],
+        [EvalErrorCode.OverSpinal, "You have equipped more spinal mount weapons than your ship's mass can accommodate."],
+        [EvalErrorCode.OverTurret, "You have equipped more turrets than your ship's mass can accommodate."],
     ]);
 </script>
 
@@ -47,7 +52,7 @@
         {:else if delta === 0}
             <span class="tag is-success">{delta} mass remaining</span>
         {:else}
-            <span class="tag is-danger">Overallocated by {delta} mass!</span>
+            <span class="tag is-danger">Overallocated by {Math.abs(delta)} mass!</span>
         {/if}
         </div>
         <div class="content">
